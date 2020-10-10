7acdac93ac89adc3f10c457910f983ae5b26cc87
Omrigo312 <omrigo312@gmail.com>
2020-10-10 03:20:11 +0300

    profile top & about components

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -17,7 +17,7 @@ const Dashboard = ({
   useEffect(() => {
     getCurrentProfile();
   }, [getCurrentProfile]);
-  return profile === null ? (
+  return loading && profile === null ? (
     <Spinner />
   ) : (
     <Fragment>
